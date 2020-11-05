3b4a3e7b3d47c7b633e0163a2ad4ea8556dce993
wilson Neto <wngaspar10@gmail.com>
2020-11-05 17:07:56 -0300

    Configuring mongoDB in the API

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,8 +1,14 @@
 import supertest from "supertest";
 import { SetupServer } from "./../src/server";
 
+let server: SetupServer;
+
 beforeAll(async () => {
-  const server = new SetupServer();
-  server.init();
+  server = new SetupServer();
+  await server.init();
   global.testRequest = supertest(server.getApp());
+});
+
+afterAll(async () => {
+  server.close();
 });
